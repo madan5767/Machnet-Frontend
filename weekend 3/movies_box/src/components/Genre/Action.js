deedaab3b0b31b4d764f8790ffe36afb9bb29cab
madan5767 <rockwithmadan@gmail.com>
2021-09-21 07:10:54 +0545

    watchlist on genre page complete

--- weekend 3/movies_box/src/components/Genre/Action.js
+++ weekend 3/movies_box/src/components/Genre/Action.js
@@ -6,6 +6,7 @@ import { useHistory } from 'react-router';
 
 function Action() {
     const [details, setDetails] = useState([]);
+    const [key, setkeys] = useState(Object.keys(localStorage));
     const history = useHistory();
 
     useEffect(()=>{
@@ -30,6 +31,21 @@ function Action() {
         <div className='first'>            
         { 
             details.map((detail) => {
+                const handelWatchListClick = () => {
+                    localStorage.setItem("wlist" + detail.id, JSON.stringify(detail));
+                    setkeys(Object.keys(localStorage));
+                };
+                const watchlist = key.includes("wlist" + detail.id);
+                const watched = key.includes("watched" + detail.id);
+                
+                const handelWatchedList = () => {
+                    localStorage.setItem("watched" + detail.id, JSON.stringify(detail));
+                    if (watchlist == true) {
+                        localStorage.removeItem("wlist" + detail.id);
+                    }
+                    setkeys(Object.keys(localStorage));
+                };
+                
                 return (        
                 <div className="item" onDoubleClick={() => movieDetailspage(detail.id)}>                    
                     {(details.length<=0)?
@@ -39,8 +55,12 @@ function Action() {
                     <h3>Movie Name:{detail.title}</h3>
                     <p>&#9734; Rating: {detail.rating}</p>
                     <p>&#9200; Duration:{detail.runtime} Mins.</p>
-                    <button>Watchlist +</button>
-                    <button>Watched &#10003; </button>
+                    {watchlist === true || watched === true ? (" ") : 
+                        (<button onClick={handelWatchListClick}>Watchlist +</button>)
+                    } 
+                    {watched === false ? (<button onClick={handelWatchedList}>Watched &#10003; </button>) :
+                        (<p>Watched &#10003; </p>)
+                    }
                     </>
                     }           
                 </div>          
